f37d4efa58d65eb1c41f65366f94237cd4d8cdb3
jlego <273734893@qq.com>
2018-11-18 00:29:00 +0800

    no message

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Wood Plugin Module.
+ * 跨域处理
+ * by jlego on 2018-11-17
+ */
+
+module.exports = app => {
+  app.application.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", app.config.crossDomain);
+    res.header("Access-Control-Allow-Headers", app.config.verifyLogin ? "Content-Type,token,secretkey" : "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
+  return app;
+}
