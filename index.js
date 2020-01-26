91f0515c2891fbf8a2da78144034c2d6ab260da7
jlego <273734893@qq.com>
2020-01-27 03:53:18 +0800

    no message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 module.exports = (app = {}, config = {}) => {
   app.application.all('*', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", config.domain || req.headers.origin);
+    res.header("Access-Control-Allow-Origin", config.domain || req.headers.origin || '*');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Headers", config.verifyLogin ? "Content-Type,token" : "Content-Type");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
